4e145796c55664d4ab80344c02d3513c1ff11459
Petr Plsek <me@kalanys.com>
2022-08-11 18:39:19 +0200

    copy only necessary files for first run, not the whole environment

--- application/first_db_run.sh
+++ application/first_db_run.sh
@@ -2,6 +2,6 @@
 # run things inside the container
 # set rights for MariaDB
 echo "Set user things"
-mysql --user=root --password=951357456852 < /application/migrations/system_user.sql
+mysql --user=root --password=951357456852 < /system_user.sql
 echo "Things in db probably set"
 # now we can migrate things
