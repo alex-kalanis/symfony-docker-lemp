3ca5a56546677d5905683b1d175d8b9cac61523e
Petr Plsek <me@kalanys.com>
2022-08-11 18:45:26 +0200

    Move sql to another dir

--- application/first_db_run.sh
+++ application/first_db_run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # run things inside the container
+# wait for MariaDB
+echo "One moment"
+sleep 10
 # set rights for MariaDB
 echo "Set user things"
 mysql --user=root --password=951357456852 < /system_user.sql
